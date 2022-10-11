eff878d28d6fad879b42f5742cd585bc6b423f0b
Edwin Rosario <edwinr9228@gmail.com>
2022-10-11 16:16:36 +0000

    Added create action to save to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
     def new 
+        @contact = Contact.new
     end
+    
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to_new_contact_path, notice= "Message Sent."
+        else 
+            redirect_to_new_contact_path, notice= "Error occured."
+        end 
+    end 
+    
+    private 
+      def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+      end 
 end 
