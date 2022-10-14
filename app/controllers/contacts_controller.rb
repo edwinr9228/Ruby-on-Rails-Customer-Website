2099d65e98a53dc58645f9ca8be451fd4944620c
Edwin Rosario <edwinr9228@gmail.com>
2022-10-14 18:38:56 +0000

    Improved flas style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,9 @@ class ContactsController < ApplicationController
     if @contact.save
        flash[:success] = "Message sent."
        redirect_to new_contact_path
+       # Ensure there's a space between redirect_to -And- new_contact_path
     else
-       flash[:error] = @contact.errors.full_messages.join(", ")
+       flash[:danger] = @contact.errors.full_messages.join(", ")
        redirect_to new_contact_path
       # Ensure there's a space between redirect_to -And- new_contact_path
     end
